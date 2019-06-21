4842b057c48f7ec5da01a9d73d889b86fde7defc
Sean Baines <mrseanbaines@gmail.com>
2019-06-21 19:46:09 +0200

    Fix Safari bug breaking copy to clipboard functionality

--- src/index.tsx
+++ src/index.tsx
@@ -49,8 +49,6 @@ class Index extends PureComponent<object, State> {
     copyTextarea.value = value;
     copyTextarea.style.position = 'fixed';
     copyTextarea.style.opacity = '0';
-    copyTextarea.style.width = '0';
-    copyTextarea.style.height = '0';
     copyTextarea.style.pointerEvents = 'none';
     document.body.appendChild(copyTextarea);
     copyTextarea.focus();
